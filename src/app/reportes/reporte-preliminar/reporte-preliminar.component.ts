c3508b04e2d4054ea308231149d66378ab4ba005
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2020-05-11 12:51:07 -0500

    se agregara filtros preliminares

--- src/app/reportes/reporte-preliminar/reporte-preliminar.component.ts
+++ src/app/reportes/reporte-preliminar/reporte-preliminar.component.ts
@@ -9,6 +9,12 @@ import { SelectItem } from 'primeng/api/selectitem';
 export class ReportePreliminarComponent implements OnInit {
   subregion=[{label:"CAJAMARCA",value:1},{label:"CHOTA",value:2},{label:"CUTERVO",value:3},{label:"JAEN",value:4}]
   subregionselect:SelectItem;
+  red=[{label:"CONTUMAZA",value:1},{label:"CAJAMARCA",value:2},{label:"CELENDIN",value:3},
+  {label:"SAN MARCOS",value:4},{label:"CAJABAMBA",value:5},{label:"SAN MIGUEL",value:6},{label:"SAN PABLO",value:7},
+  {label:"CHOTA",value:8},{label:"BAMBAMARCA",value:9},{label:"SANTA CRUZ",value:10},{label:"CUTERVO",value:11}
+  ,{label:"SOCOTA",value:12},{label:"JAEN",value:13},{label:"SAN IGNACIO",value:14}]
+  redselect:SelectItem;
+
 
   constructor() { }
 
