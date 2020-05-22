228f2d150ab8d0c5648b06204607e11ff87d9589
oitediresacajamarca <oite.diresa.cajamarca@gmail.com>
2020-05-22 11:11:19 -0500

    version 22de mayo 2020

--- src/app/configuracion/configuracion.ts
+++ src/app/configuracion/configuracion.ts
@@ -4,7 +4,7 @@ export class Configuracion {
  //  url:string="http://172.18.20.26:8088/";
 //url:string="http://172.18.21.63:8088/";
 //ip:string="localhost";
-ip:string="dwh.diresacajamarca.gob.pe";
+ip:string="hospitalvirtual.diresacajamarca.gob.pe";
 //ip:string=environment.ip;
 
 urlsimple:string="http://"+this.ip+":8089/"
--- src/app/reportes/reporte-preliminar/reporte-preliminar.component.ts
+++ src/app/reportes/reporte-preliminar/reporte-preliminar.component.ts
@@ -9,16 +9,31 @@ import { SelectItem } from 'primeng/api/selectitem';
 export class ReportePreliminarComponent implements OnInit {
   subregion=[{label:"CAJAMARCA",value:1},{label:"CHOTA",value:2},{label:"CUTERVO",value:3},{label:"JAEN",value:4}]
   subregionselect:SelectItem;
-  red=[{label:"CONTUMAZA",value:1},{label:"CAJAMARCA",value:2},{label:"CELENDIN",value:3},
-  {label:"SAN MARCOS",value:4},{label:"CAJABAMBA",value:5},{label:"SAN MIGUEL",value:6},{label:"SAN PABLO",value:7},
-  {label:"CHOTA",value:8},{label:"BAMBAMARCA",value:9},{label:"SANTA CRUZ",value:10},{label:"CUTERVO",value:11}
-  ,{label:"SOCOTA",value:12},{label:"JAEN",value:13},{label:"SAN IGNACIO",value:14}]
+
+  redes=[
+  {label:"CONTUMAZA",value:1,subregion:1},{label:"CAJAMARCA",value:2,subregion:1},{label:"CELENDIN",value:3,subregion:1},  {label:"SAN MARCOS",value:4,subregion:1},
+  {label:"CAJABAMBA",value:5,subregion:1},{label:"SAN MIGUEL",value:6,subregion:1} ,{label:"SAN PABLO",value:7,subregion:1},
+  {label:"CHOTA",value:8,subregion:2},{label:"BAMBAMARCA",value:9,subregion:2},{label:"SANTA CRUZ",value:10,subregion:2},
+  {label:"CUTERVO",value:11,subregion:3}  ,{label:"SOCOTA",value:12,subregion:3},
+  {label:"JAEN",value:13,subregion:4},  {label:"SAN IGNACIO",value:14,subregion:4}]
   redselect:SelectItem;
+  redesfiltradas=[]
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  seleccionarRed(event){
+    
+    this.redesfiltradas=this.redes.filter(data=>     
+      data.subregion==event.value
+    )
+
+ 
+  
+
   }
 
 }
